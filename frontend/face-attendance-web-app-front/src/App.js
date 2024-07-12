a222c8a630ebcc011853a07a5bef25ed54dbed9d
dardbeqiri <33403126+dardbeqiri@users.noreply.github.com>
2024-07-13 00:10:32 +0200

    implement splash screen functionality

--- frontend/face-attendance-web-app-front/src/App.js
+++ frontend/face-attendance-web-app-front/src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
-import MasterComponent from "./MasterComponent";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import MasterComponent from './MasterComponent';
+import './App.css';
+import SplashScreen from './SplashScreen';
 
-function App() {
+const App = () => {
+  const [showSplash, setShowSplash] = useState(false);
+  let timeout;
+
+  const resetTimer = () => {
+    clearTimeout(timeout);
+    setShowSplash(false);
+    timeout = setTimeout(() => {
+      setShowSplash(true);
+    }, 15000); // 15 seconds
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keydown', resetTimer);
+    window.addEventListener('mousedown', resetTimer);
+    window.addEventListener('touchstart', resetTimer);
+
+    resetTimer();
+
+    return () => {
+      clearTimeout(timeout);
+      window.removeEventListener('mousemove', resetTimer);
+      window.removeEventListener('keydown', resetTimer);
+      window.removeEventListener('mousedown', resetTimer);
+      window.removeEventListener('touchstart', resetTimer);
+    };
+  }, []);
+
   return (
     <div className="app">
+      {showSplash && <SplashScreen />}
       <MasterComponent />
     </div>
   );
-}
+};
 
 export default App;
