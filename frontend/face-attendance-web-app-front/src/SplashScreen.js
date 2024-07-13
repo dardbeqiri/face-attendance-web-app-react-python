647798e3cc0f469c45da9b5e1c3934ca0cccc50b
dardbeqiri <33403126+dardbeqiri@users.noreply.github.com>
2024-07-13 22:02:14 +0200

    A welcoming message on splashscreen

--- frontend/face-attendance-web-app-front/src/SplashScreen.js
+++ frontend/face-attendance-web-app-front/src/SplashScreen.js
@@ -6,6 +6,8 @@ const SplashScreen = () => {
   return (
     <div className="splash-screen">
       <img src={logo} alt="Logo" className="splash-logo" />
+      <center><p>Welcome to Check-in & Check out System.</p></center>
+      <center><small>Touch the screen to continue</small></center>
     </div>
   );
 };
